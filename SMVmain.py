bd664ac573472bb9f06b453f797573a8eaa801c5
jhreiser <jordanreiser1255@gmail.com>
2020-10-25 20:04:58 -0400

    Didn't go to far, just wanted to learn how to use SVM algorithm.

--- SMVmain.py
+++ SMVmain.py
@@ -1,11 +1,12 @@
 import sklearn
 from sklearn import datasets
 from sklearn import svm
+from sklearn import metrics
 
 cancer_data = datasets.load_breast_cancer()
 
-print(cancer_data.feature_names)
-print(cancer_data.target_names)
+#print(cancer_data.feature_names)
+#print(cancer_data.target_names)
 
 x = cancer_data.data
 y = cancer_data.target
@@ -17,4 +18,11 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y
 classes = ['malignant' 'benign']
 
 #implementing our classifier
-classifier = svm.SVC()
+classifier = svm.SVC(kernel="linear", C=5)
+classifier.fit(x_train, y_train)
+
+#obtaining the prediction and accuracy
+y_prediction = classifier.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, y_prediction)
+
+print(accuracy)
